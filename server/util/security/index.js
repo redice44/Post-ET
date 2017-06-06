179775685b501f0e8971d7323fba6727d3e0673a
Matt Thomson <red.cataclysm@gmail.com>
2017-06-06 12:01:42 -0400

    Modularized hashing

---
+++ server/util/security/index.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto');
+
+function hashSHA256 (str) {
+  const hash = crypto.createHash('sha256');
+  hash.update(str);
+  const sha256 = hash.digest('base64');
+  return sha256;
+}
+
+function hashUser (params) {
+  return hashSHA256(`${params.envId}${params.courseId}${params.userId}`);
+}
+
+function hashAssignment (params) {
+  return hashSHA256(`${params.envId}${params.courseId}${params.contentId}`);
+}
+
+exports.hashAssignment = hashAssignment
+exports.hashUser = hashUser
