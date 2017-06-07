d3cbafd1d8e031ac771473202e0f014944ba8909
Matt Thomson <red.cataclysm@gmail.com>
2017-06-07 10:38:11 -0400

    Learner route

--- server/routes/learner/index.js
+++ server/routes/learner/index.js
@@ -4,7 +4,12 @@ const path = require('path');
 
 router.get('/', (req, res) => {
   console.log(`Session ID: ${req.session.userId}`);
-  res.sendFile(path.join(__dirname, 'learner.html'));
+  // res.sendFile(path.join(__dirname, 'learner.html'));
+  // add variables to pass through if need be
+  const locals = {
+
+  };
+  res.render('learner/show', locals);
 });
 
 module.exports = router;
