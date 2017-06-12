d48808c0d7e73e6393d813eb5724cf98beecf037
Matt Thomson <red.cataclysm@gmail.com>
2017-06-12 00:33:03 -0400

    Learner feed page

--- server/routes/learner/index.js
+++ server/routes/learner/index.js
@@ -6,16 +6,19 @@ const path = require('path');
 
 const instagramAPI = require('../api/instagram');
 
-router.get('/', (req, res) => {
-  console.log(`Session ID: ${req.session.userId}`);
-  return res.render('learner/showAuth');
-});
+// router.get('/', (req, res) => {
+//   console.log(`Session ID: ${req.session.userId}`);
+//   return res.render('learner/showAuth');
+// });
+
+router.get('/', instagramAPI.auth.authUser);
 
 router.get('/feed', (req, res) => {
   instagramAPI.self.feed(req.session.access_token)
     .then((result) => {
       let locals = {
-        media: []
+        media: [],
+        userId: req.session.userId
       };
 
       console.log('result', result);
@@ -25,14 +28,17 @@ router.get('/feed', (req, res) => {
         if (d.type === 'image') {
           media = d.images.standard_resolution;
           media.type = 'image';
-          locals.media.push(media);
         } else if (d.type === 'video') {
           media = d.videos.standard_resolution;
           media.type = 'video';
-          locals.media.push(media);
         } else {
           console.log('Media is not an image or video. Unhandled', d);
         }
+        media.postLink = d.link;
+        media.width = Math.floor(media.width * 0.6);
+        media.height = Math.floor(media.height * 0.6);
+        media.description = d.caption.text;
+        locals.media.push(media);
       });
 
       return res.render('learner/showFeed', locals);
@@ -43,7 +49,7 @@ router.get('/feed', (req, res) => {
     });
 });
 
-router.get('/auth_user', instagramAPI.auth.authUser);
+// router.get('/auth_user', instagramAPI.auth.authUser);
 
 router.get('/handleauth', (req, res) => {
   console.log('error', req.query.error);
