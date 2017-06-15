0dc57e6de157fe33598a2c932f9b2a803d24a148
Matt Thomson <red.cataclysm@gmail.com>
2017-06-15 12:27:07 -0400

    Routes cleanup

--- server/routes/api/1.0/user/routes.js
+++ server/routes/api/1.0/user/routes.js
@@ -4,18 +4,6 @@ const mongoose = require('mongoose');
 
 const userAPI = require('./index');
 
-// router.get('/:userId', (req, res) => {
-//   userAPI.get(req.params.userId)
-//     .then((user) => {
-//       // return some display page I guess
-//       return res.send(user);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       return res.status(500).send(err);
-//     });
-// });
-
 // Submit post to assignment
 router.post('/:userId/as/:asId', (req, res) => {
   let submission = {
@@ -37,21 +25,5 @@ router.post('/:userId/as/:asId', (req, res) => {
       return res.status(500).send(err);
     });
 });
-
-// router.post('/', (req, res) => {
-//   const userData = {
-//     ID: req.body.userId,
-//     role: req.body.role
-//   };
-  
-//   userAPI.create(userData)
-//     .then((user) => {
-//       return res.json({ user: user });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       return res.status(500).send(err);
-//     });
-// });
 
 module.exports = router;
--- server/routes/learner/index.js
+++ server/routes/learner/index.js
@@ -11,8 +11,6 @@ const instagramAPI = require('../api/instagram');
 router.all('*', roleAuth.isLearner);
 
 router.get('/', (req, res) => {
-  console.log(`Session ID: ${req.session.userId}`);
-  console.log(req.session);
   assignmentAPI.get(req.session.asId)
     .then((assignment) => {
       if (!assignment) {
@@ -34,7 +32,6 @@ router.get('/', (req, res) => {
       };
 
       return res.render('learner/assignment', locals);
-      // return res.redirect(`/learner/user/${req.session.userId}/as/${req.session.asId}`)
     })
     .catch((err) => {
       console.log(err);
@@ -46,8 +43,6 @@ router.get('/', (req, res) => {
 router.get('/auth', (req, res) => {
   return res.render('learner/showAuth');
 });
-
-// router.get('/', instagramAPI.auth.authUser);
 
 router.get('/user/:userId/as/:asId', (req, res) => {
   assignmentAPI.get(req.session.asId)
@@ -61,8 +56,6 @@ router.get('/user/:userId/as/:asId', (req, res) => {
           username: req.session.instagram.username,
           assignment: assignment
         };
-
-        // console.log('result', result);
 
         result.data.forEach((d) => {
           let media = {};
@@ -91,7 +84,6 @@ router.get('/user/:userId/as/:asId', (req, res) => {
 
         delete assignment.learners;
 
-        console.log('locals for showfeed', locals);
         return res.render('learner/feed', locals);
       })
       .catch((err) => {
@@ -125,8 +117,6 @@ router.get('/authToken', (req, res) => {
 });
 
 router.get('/handleauth', (req, res) => {
-  // console.log('session', req.session);
-  // console.log('error', req.query.error);
   if (req.query.error) {
     console.log('error', req.query.error);
     console.log('reason', req.query.error_reason);
@@ -139,21 +129,6 @@ router.get('/handleauth', (req, res) => {
   };
 
   return res.render('learner/authRedirect', locals);
-
-  // instagramAPI.auth.getAuthToken(req.query.code)
-  //   .then((result) => {
-  //     console.log('result', result);
-  //     req.session.access_token = result.access_token;
-  //     req.session.instagram = result.user;
-  //     req.session.userId = result.userId;
-  //     req.session.asId = result.asId;
-
-  //     return res.redirect(`/learner/user/${result.userId}/as/${result.asId}`);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //     return res.status(500).send(err);
-  //   });
 });
 
 module.exports = router;
